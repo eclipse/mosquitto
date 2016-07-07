mqttz/mqttz

commit d9142c3974be0ef6d4bc3a0aef762f8a7d8e1f32

[476314] Deliver queued WS client msgs on reconnect.
Thanks to Joking Young and Christoph Krey.

Bug: https://bugs.eclipse.org/bugs/show_bug.cgi?id=476314

src/websockets.c
@@ -218,6 +218,8 @@ static int callback_mqtt(struct libwebsocket_context *context,
 				return -1;
 			}
 
+			mqtt3_db_message_write(db, mosq);
+
 			if(mosq->out_packet && !mosq->current_out_packet){
 				mosq->current_out_packet = mosq->out_packet;
 				mosq->out_packet = mosq->out_packet->next;

eclipse/mosquitto

commit 5edb68fa746588fe71be4901d204d2bb1a8177fd

websockets: fix compile fail after refactoring
A lot of function renaming was done in 663d50a0168bfdce98473265fdcece364600340f,
and this websockets patch was merged afterwards.  Autobuild testing
doesn't have websockets enabled, and missed this.

Fixes: d9142c3974be0ef6d4bc3a0aef762f8a7d8e1f32
Signed-off-by: Karl Palsson <karlp@etactica.com>

src/websockets.c
@@ -200,7 +200,7 @@ static int callback_mqtt(struct libwebsocket_context *context,
 				return -1;
 			}
 
-			mqtt3_db_message_write(db, mosq);
+			db__message_write(db, mosq);
 
 			if(mosq->out_packet && !mosq->current_out_packet){
 				mosq->current_out_packet = mosq->out_packet;