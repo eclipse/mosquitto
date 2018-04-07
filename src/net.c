e654b001d8d5ab9cb14ba65348bd7827774b53c9
Qubeck <qubeck@qubeck.de>
2018-04-07 16:15:41 +0200

    Fix for 'New connection from...' messages can not be disabled by 'connection_messages false'

--- src/net.c
+++ src/net.c
@@ -196,7 +196,9 @@ int net__socket_accept(struct mosquitto_db *db, mosq_sock_t listensock)
 	}
 #endif
 
-	log__printf(NULL, MOSQ_LOG_NOTICE, "New connection from %s on port %d.", new_context->address, new_context->listener->port);
+	if (db->config->connection_messages == true) {
+		log__printf(NULL, MOSQ_LOG_NOTICE, "New connection from %s on port %d.", new_context->address, new_context->listener->port);
+	}
 
 	return new_sock;
 }
