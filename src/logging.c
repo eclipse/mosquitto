2ef4866d1c05f0f3fdcd642d47ef859a1c1e5f2d
Tilman Blumhagen <tilman.blumhagen@googlemail.com>
2023-05-09 08:46:55 +0200

    Fixed Windows-build, there is no setlinebuf

--- src/logging.c
+++ src/logging.c
@@ -130,9 +130,11 @@ int log__init(struct mosquitto__config *config)
 			log__printf(NULL, MOSQ_LOG_ERR, "Error: Unable to open log file %s for writing.", config->log_file);
 		}
 	}
+#ifndef WIN32
 	if(log_destinations & MQTT3_LOG_STDOUT){
 		setlinebuf(stdout);
 	}
+#endif
 #ifdef WITH_DLT
 	dlt_fifo_check();
 	if(dlt_allowed){
