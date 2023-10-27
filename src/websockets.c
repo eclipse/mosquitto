86d70b5d0e45c85d8ba61ce087ac5539ef0e00df
Oleg Kisseloff <olegkisselef@gmail.com>
2023-10-27 13:43:09 +0100

    - enabling unix domain sockets (UDS) mode for lws instance if given port is 0 - passing UDS filename instead of non-existent host name to lws instance
    Signed-off-by: Oleg Kisseloff <olegkisselef@gmail.com>

--- src/websockets.c
+++ src/websockets.c
@@ -687,7 +687,12 @@ void mosq_websockets_init(struct mosquitto__listener *listener, const struct mos
 	}
 
 	memset(&info, 0, sizeof(info));
-	info.iface = listener->host;
+	if(listener->port == 0){
+		info.iface = listener->unix_socket_path;
+		info.options |= LWS_SERVER_OPTION_UNIX_SOCK;
+	}else{
+		info.iface = listener->host;
+	}
 	info.port = listener->port;
 	info.protocols = p;
 	info.gid = -1;
